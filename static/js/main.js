b30e98a9a4d6468e79aeb9b90b2b17bbaf4231e7
kasugamirai <xyzmhx@gmail.com>
2023-01-16 04:13:41 +0900

    1

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,18 @@ window.onload = function(){
     var data
     var url = "/datazh"
     fetch(url).then(response => data)
-    var l = data.length
-    alert(l)
-     
+    JSON.parse(data)
+    Pokemon_zh, Abilities_zh, Types_zh, Natures_zh, Items_zh, Moves_zh
+    var arr=new Array('Pokemon', 'Ability', 'TeraType','Nature','Item', 'Move_1', 'Move_2', 'Move_3', 'Move_4')
+    function addopt(){
+                for(var i=0; i < 6; i++) {                    
+                    for(var j=0; j < 6; j++) {
+                        var opt=document.createElement('option')
+                        opt.text = data[i][1]
+                        opt.value = data[1][1]
+                        document.getElementById(arr[i]).add(opt)
+                    }                   
+                }
+    }
+    addopt()
 }
