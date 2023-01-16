db0e339c8b81a076628f8c9550b4b45732d3307a
kasugamirai <xyzmhx@gmail.com>
2023-01-17 01:15:52 +0900

    update

--- static/js/main.js
+++ static/js/main.js
@@ -3,17 +3,17 @@ window.onload = function(){
     var url = "/datazh"
     fetch(url).then(data => data.JSON)
     JSON.parse(data)
-    Pokemon_zh, Abilities_zh, Types_zh, Natures_zh, Items_zh, Moves_zh
+    data = data.append(data[5])
+    data = data.append(data[5])
+    data = data.append(data[5])
     var arr=new Array('Pokemon', 'Ability', 'TeraType','Nature','Item', 'Move_1', 'Move_2', 'Move_3', 'Move_4')
     function addopt(){
                 for(var i=0; i < 9; i++) {                    
-                    for(var j=0; j < 6; j++) {
-                        for(var x=0; x < data.length; x++) {
-                            var opt=document.createElement('option')
-                            opt.text = data[j][x]
-                            opt.value = data[j][x]
-                            document.getElementById(arr[i]).add(opt)
-                        }                        
+                    for(var j=0; j < data[i].length; j++) {
+                        var opt=document.createElement('option')
+                        opt.text = data[i][j]
+                        opt.value = data[i][j]
+                        document.getElementById(arr[i]).add(opt)
                     }                   
                 }
     }
